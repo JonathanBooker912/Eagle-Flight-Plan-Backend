422551e9b7bc9a3164132a326bce31d2771793b5
Yorsh <george.hartman@eagles.oc.edu>
2025-03-28 15:38:39 -0500

    lint

--- app/routes/strength.routes.js
+++ app/routes/strength.routes.js
@@ -1,5 +1,4 @@
 import strengths from "../controllers/strength.controller.js";
-import { authenticate } from "../authorization/authorization.js";
 import { Router } from "express";
 
 const router = Router();
